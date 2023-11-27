d5b446988d420a9bc78e18d40a5b2bd03d7fbbfe
Grinchs <marcisg2345@gmail.com>
2023-11-28 00:37:31 +0200

    Add project files.

---
+++ 7_cikli_1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_cikli_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool x = true;
+            do
+            {
+                Console.WriteLine("Ievadiet skaitli no 1 līdz 5 vai IZIET lai beigtu darbību");
+
+                string viens = Console.ReadLine();
+
+                switch (viens)
+                {
+                    case "1":
+                        Console.WriteLine("Viens");
+                        break;
+
+                    case "2":
+                        Console.WriteLine("Divi");
+                        break;
+
+                    case "3":
+                        Console.WriteLine("Trīs");
+                        break;
+
+                    case "4":
+                        Console.WriteLine("Četri");
+                        break;
+
+                    case "5":
+                        Console.WriteLine("Pieci");
+                        break;
+
+                    case "IZIET":
+                        x = !x;
+                        break;
+                }
+                
+            }
+            while (x);
+        }
+
+    }
+}    
